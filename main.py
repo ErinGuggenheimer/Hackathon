8bc75c51a712b5dfe2ee28baeb3606c1eb7fde1b
Erin <eringuggenheimer1@gmail.com>
2024-01-20 01:44:29 -0800

    Profile 1-3 plus contact plus policy
    added comment to test git
    made by the Gugg

--- main.py
+++ main.py
@@ -10,12 +10,21 @@ def deliver_home_page_to_user():
 
 
 
+@app.route('/contact')
+def deliver_contact_page_to_user():
+  return render_template('Contact.html')
+
+
+@app.route('/policy')
+def deliver_policy_page_to_user():
+  return render_template('policy.html')
+
 
 @app.route('/tasksearch')
 def deliver_tasksearch_page_to_user():
   return render_template('tasksearch.html')
 
-@app.route('/message')
+@app.route('/message?#')
 def deliver_message_page_to_user():
   return render_template('message.html')
 
@@ -24,9 +33,24 @@ def deliver_register_page_to_user():
   return render_template('register.html')
 
 
+@app.route('/cemessage?#')
+def deliver_profile1_page_to_user():
+  return render_template('profile1.html')
+
+
+@app.route('/kmmessage?#')
+def deliver_profile2_page_to_user():
+  return render_template('profile2.html')
+
+
+
+@app.route('/jdmessage?#')
+def deliver_profile3_page_to_user():
+  return render_template('profile3.html')
+
 @app.route('/login')
 
-def deliver_login_page_to_user():
+def deliver_profile3_page_to_user():
   return render_template('login.html')
 
 @app.route('/login' , methods=['POST'])
